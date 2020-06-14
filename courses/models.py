332b7d6d1c5a7c181307761387266435779999f9
Piravin <piravin.kcm@gmail.com>
2020-06-14 18:32:53 +0530

    modified:   courses/models.py

--- courses/models.py
+++ courses/models.py
@@ -12,3 +12,14 @@ class Course(models.Model):
 class Topic(models.Model):
     name = models.CharField(max_length=100)
     parent_course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    class __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, default=1, on_delete=models.SET_DEFAULT)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    class __str__(self):
+        return self.author
