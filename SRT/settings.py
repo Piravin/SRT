a8c91309d1ce3e4ef20952302b3c72273be9b3a3
Piravin <piravin.kcm@gmail.com>
2020-06-21 09:43:32 +0530

    after mano error

--- SRT/settings.py
+++ SRT/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://srtbaja.herokuapp.com']
+ALLOWED_HOSTS = ['https://srtbaja.herokuapp.com','192.168.137.1']
 
 
 # Application definition
